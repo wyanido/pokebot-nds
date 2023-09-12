86ae5dbdfe7540f76072b464d64e1e554a20734e
NIDO <80712417+wyanido@users.noreply.github.com>
2023-09-12 18:05:34 +0100

    Update start-dashboard.bat

--- start-dashboard.bat
+++ start-dashboard.bat
@@ -1,3 +1,15 @@
-cd dashboard
-npm start
-pause
+@echo off
+setlocal
+
+node -v >nul 2>&1
+if %errorlevel% equ 0 (
+    cd dashboard
+    npm install
+    cd dashboard
+    npm start
+) else (
+    echo Node.js is required to launch the dashboard without a standalone binary.
+    echo Please download and install Node.js from https://nodejs.org/en/download
+)
+
+cmd /k
